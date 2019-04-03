bc3ef182e14418f3d4951f6d2dbac63309fb3b53
gemking <vivas5000@gmail.com>
2019-04-02 21:48:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,141 +1,34 @@
-    
 <?php
-session_start();
+ini_set('display_errors', '1');
+session_start(); // Connect to the existing session
 require_once '/home/common/mail.php'; // Add email functionality
-require_once '/home/common/dbInterface.php'; // Add database functionality
-processPageRequest();
+require_once'/home/common/dbInterface.php'; // Add database functionality
+processPageRequest(); // Call the processPageRequest() function
+
 function addMovieToCart($movieID){
-	$id = movieExistsInDB($movieID);
-	if($id === 0){
-		$movie = file_get_contents('http://www.omdbapi.com/?apikey=89270bfb&i=' . $moveID . '&type=movie&r=json');
-		$array = json_decode($movie, true);
-		$id = addMovie($array['imdbId'], $array['title'], $array['year'], $array['rating'], $array['runtime'], $array['genre'], $array['actors'], $array['director'], $array['writer'], $array['plot'], $array['poster']);
-	}
-	addMovieToShoppingCart($_SESSION["userId"], $id);
-	displayCart();
-}
-function checkout($name, $address){
-	$subject = "Your Receipt from myMovies!";
-	$message = mailMessage();
-	$encode = urlencode($message);
-	$message = "
-	<h3>Your transaction was successful.</h3>
-	<p>You have successful purchase the items below from our store</p>
-	<p>$encode</p>
-	<p>If you have any question or complaint, please reach out to us via the customer line. Thank you</p>";
-	$header = "From:cse@test.com \r\n";
-    $header .= "Cc:cse@test.com \r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type: text/html\r\n";
-	$result = mail($address, $subject, $message, $header);
-	return $result;
-}
-function createMovieList($forEmail=false){
-	$movieIds = isset($_SESSION['order']) ? getMoviesInCart($_SESSION['userId'], $_SESSION['order']) : getMoviesInCart($_SESSION['userId']);
-	$string = '<table border="2" style="padding: 10px;">
-	<thead>
-	<tr>
-	<th>Movie Image</th>
-	<th>Movie title</th>
-	<th>Remove Item</th>
-	</tr>
-	</thead>
-	<tbody>';
-	for ($i=0; $i < count($movieIds); $i++) {
-		$movie = getMovieData($movieIds[i]);
-		$title = str_replace("'", " ", $array["Title"]);
-		$image = $array["Poster"];
-		$year = $array["Year"];
-		$string .= '<tr>';
-		$string .= '<td><img src="'.$image.'" height="100px" /></td>';
-		$string .= '<td><h3>'.$title.' ('.$year.')</h3></td>';
-		$string .= $forEmail ? '' : '<a href="javascript:void(0);" onclick=\'displayMovieInformation([movie_id]);\'>View More Info</a>';
-		$string .= $forEmail ? '' : "<td><a href='#' onclick='confirmRemove(\"".$title."\", \"".$movie['ID']."\")'>X</a></td>";
-		$string .= '</tr>';
-	}
-	$string .='</tbody></table>';
-	return $string;
-}
-function displayCart($forEmail = false){
-	$movieCount = countMoviesInCart($_SESSION["userId"]);
-	echo '
-    <!DOCTYPE html>
-    <html>
-    <head>
-			' . ($forEmail ? '' :
-				'<title>Home - myXpress Movies!</title>
-				<script src="script.js"></script>')
-    . '</head>
-    <body>
-    <h1>myMovies Xpress!</h1>
-    ' . ($forEmail ? '' : '<div style="text-align: left" class="">
-    	<p>Welcome '.$_SESSION["name"].' (<a href="#" onclick="confirmLogout()">logout</a>)</p> </div>
-	') ;
-	if($movieCount > 0){
-		$apiKey = "c37e4353";
-		echo '<p>' . $movieCount . ' Movies in your shopping cart</p>
-			<select id=\'select_order\' onchange=\'changeMovieDisplay();\'>
-				<option value="0">Movie Title</option>
-				<option value="1">Runtime (shortest -­> longest)</option>
-				<option value="2">Runtime (longest ­-> shortest)</option>
-				<option value="3">Year (old -­> new)</option>
-				<option value="4">Year (new -­> old)</option>
-			</select>
-			<div id=\'shopping_cart\'>'. createMovieList($forEmail) . '</div>'; 
-		$redirect = "window.location.href='./search.php'";
-		echo '
-			<br>
-			' . ($forEmail ? '' : '<button onclick="'.$redirect.'">Add Movie</button><br><br>
-			<button onclick="confirmCheckout()">Checkout</button>
-    ');
-	} else {
-		$redirect = "window.location.href='./search.php'";
-		echo'<p>Add Some Movies to Your Cart</p>
-		<br>
-		<button onclick="'.$redirect.'">Add Movie</button><br><br>
-		';
-	}
-	echo ($forEmail ? '' : '<div id=\'modalWindow\' class=\'modal\'><div id=\'modalWindowContent\' class=\'modal-content\'></div></div>') . '</body></html>';
+    $movieId =  movieExistsInDB($movieID);
+    if($movieId === 0){
+    	$movie = file_get_contents('http://www.omdbapi.com/?apikey=89270bfb&i='.$movieID.'&type=movie&r=json');
+    	$array = json_decode($movie, true);
+	print_r($array);
+    	$id = addMovie($array['imdbID'], $array['Title'], $array['Year'], $array['imdbRating'], $array['Runtime'], $array['Genre'], $array['Actors'], 
+        $array['Director'], $array['Writer'], $array['Plot'], $array['Poster']);
+    }
+
+    addMovieToShoppingCart($_SESSION["ID"], $movieID);
+    displayCart();	
 }
 
-function processPageRequest(){
-	if(!isset($_GET["action"])){
-		displayCart();
-	} else {
-		if($_GET["action"] == "add"){
-			$movieID = $_GET["movie_id"]; 
-			addMovieToCart($movieId);
-		} else if ($_GET["action"] == "checkout") {
-			checkout($_SESSION["displayName"], $_SESSION["emailAddress"]);
-			displayCart(); 
-		} else if ($_GET["action"] == "remove") {
-			$movieID = $_GET["movie_id"];
-			removeMovieFromCart($movieID);
-		} else if($_GET["action"] == "update"){
-			updateMovieListing($_GET["order"]);
-		}
-	}
-}
-
-function removeMovieFromCart($movieID){
-	removeMovieFromShoppingCart($_SESSION['userId'], $movieID);
-	displayCart();
-}
-function updateMovieListing($order){
-	$_SESSION['order'] = $order;
-	echo createMovieList(true);
-}
-function mailMessage(){
+function encoder(){ 
 	$filename = "./data/cart.db";
 	$encode = "";
 	$rowList;
 	$rowList = array();
+	
 	 $file = fopen( $filename, "r" );
-    if( $file == false ) {
-		echo ( "Error in opening file" );
-        exit();
-    }
-    $filesize = filesize( $filename );
+
+     
+    $filesize = filesize( $filename ); 
 	if($filesize !== 0){
 		while(!feof($file))
 		  {
@@ -145,8 +38,9 @@ function mailMessage(){
 				array_push($rowList, $text);
 			}
 		  }
-	}
-    fclose($file);
+	} 
+    fclose($file);  
+	
     $encode = $encode . '
     <!DOCTYPE html>
     <html>
@@ -162,9 +56,10 @@ function mailMessage(){
 			margin: 0 auto;
 		}
 	</style>
-    <h1>myMovies Xpress!</h1>
+    <h1>myMovies Xpress!</h1> 
 ';
-	if(count($rowList) > 0 && filesize($filename) && !empty($rowList)){
+	 
+	if(count($rowList) > 0 && filesize($filename) && !empty($rowList)){ 
 		$apiKey = "89270bfb";
 		$encode = $encode . '
 			<table border="1">
@@ -172,32 +67,159 @@ function mailMessage(){
 			<tr>
 			<th>Movie Image</th>
 			<th>Movie title</th>
-			<th>Remove Item</th>
 			</tr>
 			</thead>
-			<tbody>';
-		for ($i=0; $i < count($rowList); $i++) {
-			$movie_id = trim($rowList[$i]);
+			<tbody>'; 
+		for ($x = 0; $x < count($rowList); $x++) { 
+			$movie_id = trim($rowList[$x]);
 			$movie = file_get_contents("http://www.omdbapi.com/?i=$movie_id&apikey=$apiKey&type=movie&r=json");
 			$array = json_decode($movie, true); 
-			$title = $array["Title"];
-			$image = $array["Poster"];
-			$year = $array["Year"];
+			$title = $array["Title"]; 
+			$image = $array["Poster"]; 
+			$year = $array["Year"]; 
 			$url = "https://www.imdb.com/title/$movie_id/";
-			$encode = $encode .  '<tr>';
+			$encode = $encode .  '<tr>'; 
 			$encode = $encode .  '<td><img src="'.$image.'" height="100px" /></td>';
 			$encode = $encode . '<td><h3><a href="'.$url.'" targer="_blank">'.$title.' : '.$year.'</a></h3></td>';
-			$encode = $encode .  "<td><a href='#' onclick='confirmRemove(\"".$title."\", \"".$movie_id."\")'>X</a></td>";
 			$encode = $encode . '</tr>';
 		}
 		$redirect = "window.location.href='./search.php'";
 		$encode = $encode .  '
 			</tbody>
-			</table>
+			</table> 
     </body>
     </html>
-    ';
+    ';  
     return $encode;
+	}  
+}
+
+
+
+
+function checkout($name, $address){  
+	$subject = "Your Receipt from myMovies!";
+	$message = encoder();
+	$address = "n01052448@ospreys.unf.edu"; 
+	$decode = urldecode($message);
+	$name = "bobthebuilder"; 
+	$message = "
+	<h3>Your transaction was successful.</h3>
+	<p>You have successfully purchased the items below from our store</p>
+	<p>$decode</p>
+	<p>If u have any inquiries regarding your transaction, please contact our customer service support</p>";
+	$result = sendMail(555409699, $address, $name, $subject, $message); 
+	return $result;
+}
+
+function createMovieList($forEmail = false){
+	$movieIds = isset($_SESSION['order']) ? getMoviesInCart($_SESSION['userId'], $_SESSION['order']) : getMoviesInCart($_SESSION['userId']);
+	$string = '<table border="3" style="padding: 15px;">
+	<thead>
+	<tr>
+	<th>Movie Image</th>
+	<th>Movie title</th>
+	<th>Remove Item</th>
+	</tr>
+	</thead>
+	<tbody>';
+	for ($x = 0; $x < count($movieIds); $x++) {
+		$movie = getMovieData($movieIds[x]);
+		$title = str_replace("'", " ", $array["Title"]);
+		$image = $array["Poster"];
+		$year = $array["Year"];
+		$string .= '<tr>';
+		$string .= '<td><img src="'.$image.'" height="100px" /></td>';
+		$string .= '<td><h3>'.$title.' ('.$year.')</h3></td>';
+		$string .= $forEmail ? '' : '<a href="javascript:void(0);" onclick=\'displayMovieInformation([movie_id]);\'>View More Info</a>';
+		$string .= $forEmail ? '' : "<td><a href='#' onclick='confirmRemove(\"".$title."\", \"".$movie['ID']."\")'>X</a></td>";
+		$string .= '</tr>';
+	}
+	$string .='</tbody></table>';
+	return $string;
+}
+
+function displayCart($forEmail = false){
+	$movieCount = countMoviesInCart($_SESSION["userId"]);
+    echo '
+    <!DOCTYPE html>
+    <html>
+    <head>
+    	<title>Home - myXpress Movies!</title>
+    	<script src="script.js"></script>
+    </head>
+    <body>
+    <h1>myMovies Xpress!</h1>
+    <div style="text-align: left" class="">
+    	<p>Welcome, '.$_SESSION["displayName"].' </p>
+    	<p><a href="#" onclick="confirmLogout()">Logout</a></p> 
+    </div>
+';
+	if($movieCount > 0){
+		$apiKey = "89270bfb";
+		echo '<p>' . $movieCount . ' Movies in your shopping cart</p>
+		<select id=\'select_order\' onchange=\'changeMovieDisplay();\'>
+			<option value="0">Movie Title</option>
+			<option value="1">Runtime (shortest -­> longest)</option>
+			<option value="2">Runtime (longest ­-> shortest)</option>
+			<option value="3">Year (old -­> new)</option>
+			<option value="4">Year (new -­> old)</option>
+		</select>
+			<div id=\'shopping_cart\'>'. createMovieList($forEmail) . '</div>'; 
+		
+		$redirect = "window.location.href='./search.php'";
+		echo '
+			<br>
+			' . ($forEmail ? '' : '<button onclick="'.$redirect.'">Add Movie</button><br><br>
+			<button onclick="confirmCheckout()">Checkout</button>');
+	} else {
+		$redirect = "window.location.href='./search.php'";
+		echo'<p>Add Some Movies to Your Cart</p>
+		<br/>
+		<button onclick="'.$redirect.'">Add Movie</button><br/><br/>';
+	}
+	echo ($forEmail ? '' : '<div id=\'modalWindow\' class=\'modal\'><div id=\'modalWindowContent\' class=\'modal-content\'></div></div>') . '</body></html>';
+} 
+
+
+function processPageRequest(){
+	if(!isset($_SESSION['displayName'])){
+		header('logon.php');
+		
+	}
+	if(isset($_GET["action"])){
+		switch($_GET["action"]){
+			case "add":
+				$movieID = $_GET["movie_id"];
+				addMovieToCart($movieID);
+				break;
+			case "checkout":
+				checkout($_SESSION["name"], $_SESSION["emailAddress"]);
+				break;
+			case 'remove':
+				$movieID = $_GET["movie_id"];
+				removeMovieFromCart($movieID);
+				break;
+			case 'update':
+				$order = $_GET['order'];
+				updateMovieListing($order);
+				break;
+			default:
+				break;
+		}
+	} else {
+		displayCart();
 	}
 }
-?>
+
+
+
+function removeMovieFromCart($movieID){
+	removeMovieFromShoppingCart($_SESSION['userId'], $movieID);
+	displayCart();
+}
+
+function updateMovieListing($order){
+	$_SESSION['order'] = $order;
+	echo createMovieList(true);
+}
